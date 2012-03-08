b1c88ae461b494c4b5635a4ada3ca495daf842a9
Yoshio Nishi <s031746@gmail.com>
2012-03-08 17:41:23 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialrealestate::Application.config.secret_token = '4ed38b3e924ec87425379502255b58368e05697bfcc8eb2d9302a692f4d5d1f05d193f6f4d89eb87077022944b6b5b28ece99d30656506a6e8bf99b2690fdd4a'
